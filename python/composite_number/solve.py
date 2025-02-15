d02a168b3709561f2b4fce866a5e65a64ab39cdb
Ivan Kalashnikov <ivank.public@gmail.com>
2025-02-16 00:29:21 +0700

    Задача "number composite" - добавление реализаций через подход динамического программирования.

--- python/composite_number/solve.py
+++ python/composite_number/solve.py
@@ -1,3 +1,5 @@
+# Classis solution  - algo: O(2^n/max); memory: O(1)
+
 def is_number_composite_3_5(num: int) -> bool:
   if num <= 0:
     return False
@@ -11,6 +13,26 @@ def is_number_composite_3_5(num: int) -> bool:
 
   return False
 
+# Dynamic solution - algo: O(n); memory: O(n)
+
+mem_composite_3_5_dynamic: dict[int, bool] = {}
+
+def is_number_composite_3_5_dynamic(num: int) -> bool:
+  if num <= 0:
+    return False
+  if (num % 3 == 0) or (num % 5 == 0):
+    mem_composite_3_5_dynamic[num] = True
+    return True
+
+  if num >= 5:
+    return is_number_composite_3_5(num - 5)
+  if num >= 3:
+    return is_number_composite_3_5(num - 3)
+
+  mem_composite_3_5_dynamic[num] = False
+  return False
+
+# Classis solution - algo: O(2^n/max); memory: O(1)
 
 def is_number_composite_of(num: int, composite_of: set[int]) -> bool:
   if num <= 0:
@@ -28,3 +50,36 @@ def is_number_composite_of(num: int, composite_of: set[int]) -> bool:
     return False
 
   return is_number_composite_of(num - max_elem, composite_of)
+
+# Dynamic solution - algo: O(n); memory: O(n)
+
+g_mem_composite_of_dynamic: dict[int, dict[frozenset, bool]] = {}
+
+def is_number_composite_of_dynamic(num: int, composite_of: set[int]) -> bool:
+  if num <= 0:
+    return False
+
+  frozen_composite_of = frozenset(composite_of)
+
+  if num in g_mem_composite_of_dynamic and frozen_composite_of in g_mem_composite_of_dynamic[num]:
+    return g_mem_composite_of_dynamic[num][frozen_composite_of]
+
+  for elem in composite_of:
+    if (num % elem == 0):
+      g_mem_composite_of_dynamic.setdefault(num, {})[frozen_composite_of] = True
+      return True
+
+  max_elem: int = max(composite_of)
+  if max_elem > num:
+    new_composite_of = composite_of - {max_elem}
+    if new_composite_of:
+      result = is_number_composite_of_dynamic(num, new_composite_of)
+      g_mem_composite_of_dynamic.setdefault(num, {})[frozen_composite_of] = result
+      return result
+
+    g_mem_composite_of_dynamic.setdefault(num, {})[frozen_composite_of] = False
+    return False
+
+  result = is_number_composite_of_dynamic(num - max_elem, composite_of)
+  g_mem_composite_of_dynamic.setdefault(num, {})[frozen_composite_of] = result
+  return result
--- python/composite_number/test_solve.py
+++ python/composite_number/test_solve.py
@@ -1,72 +1,97 @@
 import pytest
 
-from python.composite_number.solve import is_number_composite_3_5, is_number_composite_of
+from python.composite_number.solve import (
+  is_number_composite_3_5, is_number_composite_3_5_dynamic,
+  is_number_composite_of, is_number_composite_of_dynamic)
 
-@pytest.mark.parametrize(
-  "num",
-  [
-    3, 5, 15, 17
-  ]
-)
+
+normal_positive_data_composite_3_5: list[int] = [3, 5, 15, 17, 121, 122, 558, 621, 11111111]
+
+@pytest.mark.parametrize("num", normal_positive_data_composite_3_5)
 def test_normal_positive_is_number_composite_3_5(num):
   assert is_number_composite_3_5(num) == True
 
-@pytest.mark.parametrize(
-  "num",
-  [
-    1, 2, 4, 7
-  ]
-)
+
+@pytest.mark.parametrize("num", normal_positive_data_composite_3_5)
+def test_normal_positive_is_number_composite_3_5_dynamic(num):
+  assert is_number_composite_3_5_dynamic(num) == True
+
+
+normal_negative_data_composite_3_5: list[int] = [1, 2, 4, 7]
+
+@pytest.mark.parametrize("num", normal_negative_data_composite_3_5)
 def test_normal_negative_is_number_composite_3_5(num):
   assert is_number_composite_3_5(num) == False
 
-@pytest.mark.parametrize(
-  "num",
-  [
-    0, -2, -3, -5
-  ]
-)
+@pytest.mark.parametrize("num", normal_negative_data_composite_3_5)
+def test_normal_negative_is_number_composite_3_5_dynamic(num):
+  assert is_number_composite_3_5_dynamic(num) == False
+
+
+edge_negative_data_composite_3_5: list[int] = [0, -2, -3, -5]
+
+@pytest.mark.parametrize("num", edge_negative_data_composite_3_5)
 def test_edge_negative_is_number_composite_3_5(num):
   assert is_number_composite_3_5(num) == False
 
 
-@pytest.mark.parametrize(
-  ("num", "composite_of"),
-  [
-    (3, {3, 5}),
-    (15, {3, 5}),
-    (17, {3, 5}),
-    (17, {2, 3, 5}),
-    (15, {2, 3}),
-    (18, {2, 3})
-  ]
-)
+@pytest.mark.parametrize("num", edge_negative_data_composite_3_5)
+def test_edge_negative_is_number_composite_3_5_dynamic(num):
+  assert is_number_composite_3_5_dynamic(num) == False
+
+
+normal_positive_data_composite_of: list[tuple[int, set[int]]] = [
+  (3, {3, 5}),
+  (15, {3, 5}),
+  (17, {3, 5}),
+  (17, {2, 3, 5}),
+  (15, {2, 3}),
+  (18, {2, 3}),
+  (20, {5, 10}),
+  (155, {5, 10})
+]
+
+@pytest.mark.parametrize(("num", "composite_of"), normal_positive_data_composite_of)
 def test_normal_positive_is_number_composite_of(num, composite_of):
   assert is_number_composite_of(num, composite_of) == True
 
-@pytest.mark.parametrize(
-  ("num", "composite_of"),
-  [
-    (1, {3, 5}),
-    (2, {3, 5}),
-    (4, {3, 5}),
-    (7, {3, 5})
-  ]
-)
+@pytest.mark.parametrize(("num", "composite_of"), normal_positive_data_composite_of)
+def test_normal_positive_is_number_composite_of_dynamic(num, composite_of):
+  assert is_number_composite_of_dynamic(num, composite_of) == True
+
+
+normal_negative_data_composite_of: list[tuple[int, set[int]]] = [
+  (1, {3, 5}),
+  (2, {3, 5}),
+  (4, {3, 5}),
+  (7, {3, 5}),
+  (32, {5, 10}),
+  (122, {5, 10})
+]
+
+@pytest.mark.parametrize(("num", "composite_of"), normal_negative_data_composite_of)
 def test_normal_negative_is_number_composite_of(num, composite_of):
   assert is_number_composite_of(num, composite_of) == False
 
-@pytest.mark.parametrize(
-  ("num", "composite_of"),
-  [
-    (0, {3, 5}),
-    (-2, {3, 5}),
-    (-3, {3, 5}),
-    (-5, {3, 5})
-  ]
-)
+@pytest.mark.parametrize(("num", "composite_of"), normal_negative_data_composite_of)
+def test_normal_negative_is_number_composite_of_dynamic(num, composite_of):
+  assert is_number_composite_of_dynamic(num, composite_of) == False
+
+
+edge_negative_data_composite_of: list[tuple[int, set[int]]] = [
+  (0, {3, 5}),
+  (-2, {3, 5}),
+  (-3, {3, 5}),
+  (-5, {3, 5})
+]
+
+@pytest.mark.parametrize(("num", "composite_of"), edge_negative_data_composite_of)
 def test_edge_negative_is_number_composite_of(num, composite_of):
   assert is_number_composite_of(num, composite_of) == False
+
+@pytest.mark.parametrize(("num", "composite_of"), edge_negative_data_composite_of)
+def test_edge_negative_is_number_composite_of_dynamic(num, composite_of):
+  assert is_number_composite_of_dynamic(num, composite_of) == False
 
 
 if __name__ == "__main__":
